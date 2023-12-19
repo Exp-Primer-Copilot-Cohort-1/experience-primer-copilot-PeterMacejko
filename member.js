87d45c5e1bdef173ad98ae01c5a583406a8e87b3
PeterMacejko <116878913+PeterMacejko@users.noreply.github.com>
2023-12-19 00:10:48 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    let skills = ['HTML', 'CSS', 'JS'];
+    let member = {
+        name: 'Lee',
+        age: 25,
+        skills: skills
+    };
+    console.log(member);
+}
